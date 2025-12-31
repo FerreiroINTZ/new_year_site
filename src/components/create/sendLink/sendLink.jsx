3b0fad5a028f5590f0e3db359d99ae7fb6c81f52
gabriel felipe <gabrielferreiro963@gmail.com>
2025-12-31 18:27:27 -0300

    fixed: opne link as not funcioando

--- src/components/create/sendLink/sendLink.jsx
+++ src/components/create/sendLink/sendLink.jsx
@@ -22,7 +22,7 @@ function sendLink() {
       <div id={style.sendLink} onClick={() => setShareLinkContainer(false)}>
       <div id={style.card} onClick={e => e.stopPropagation()}>
         <h1>Seu Link foi criado com <br /><span>sucesso!</span></h1>
-        <div id={style.linkWrapper}><a href={shareLink} target="_blank">ACESSAR LINK</a></div>
+        <div id={style.linkWrapper}><a href={`https://${shareLink}`} target="_blank" >ACESSAR LINK</a></div>
         <div id={style.actionsBtnsWrapper}>
           <button id={style.shareBtn} onClick={share}>Compartilhar</button>
           <button id={style.coppyBtn} onClick={() => navigator.clipboard.readText(shareLink)}>COPIAR</button>
